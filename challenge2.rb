d2273c51a40b53b243deb6c6f7c1c3311aea97b9
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-06-27 15:02:19 -0600

    Complete challenge two

---
+++ challenge2.rb
@@ -0,0 +1,13 @@
+ # create an array
+ # create strings with mix of lowercase and uppercase
+ # print every word in lowercase 
+
+
+ # create an array with strings with mix of upper and lowercase letters
+ strings = ["pEn", "pApEr", "ERASER", "Light"]  
+
+ strings.each do |string| # iterate over all elements in array
+    puts string.downcase # call downcase method on all elements and print them
+ end
+
+
