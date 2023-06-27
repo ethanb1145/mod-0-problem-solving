d858e3abfb7708d18267d553516b719563505272
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-06-27 15:01:10 -0600

    Add correction

--- challenge1.rb
+++ challenge1.rb
@@ -8,7 +8,7 @@ strings = ["House", "Car", "Bank", "City", "Dog", "Phone"]
 
 # create a method to iterate over array
 strings.each do |string|  
-    if string.length == 4 # checks each element if character length is 4
+    if string.length == 4 # check each element if character length is 4, call length method on the strings
         puts string      # output element(s) if true
     end
     
